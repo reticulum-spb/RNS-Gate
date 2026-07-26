7045f1828081778abc200b2de09ca89dd2394128
Belousov Oleg <belousov.oleg@gmail.com>
2026-07-26 15:57:30 +0300

    Added rs-rcc-client, rs-nomadnet packages

---
+++ Firmware/system/br2_external/package/rs-rrc-client/rs-rrc-client.mk
@@ -0,0 +1,17 @@
+################################################################################
+#
+#  rsRRC-client
+#
+################################################################################
+
+RS_RRC_CLIENT_VERSION = c31425dd3a5fd5aba66528322cfaf84d4fa8665d
+RS_RRC_CLIENT_SITE = https://github.com/reticulum-spb/rsRRC-client
+RS_RRC_CLIENT_SITE_METHOD = git
+
+define RS_RRC_CLIENT_CREATE_SYMLINK
+    ln -sfn $(@D) $(BUILD_DIR)/rsRRC-client
+endef
+
+RS_RRC_CLIENT_POST_EXTRACT_HOOKS += RS_RRC_CLIENT_CREATE_SYMLINK
+
+$(eval $(generic-package))
