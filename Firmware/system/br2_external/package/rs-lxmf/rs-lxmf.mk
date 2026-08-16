b9a07fdcef3ae58359b646ff45df125daa5dd95e
Belousov Oleg <belousov.oleg@gmail.com>
2026-08-16 14:26:29 +0300

    Fixes, update rs-lxmf

--- Firmware/system/br2_external/package/rs-lxmf/rs-lxmf.mk
+++ Firmware/system/br2_external/package/rs-lxmf/rs-lxmf.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-RS_LXMF_VERSION = 3cbd0b19c626d8ab1bb9aa00c61e12a08f786e44
+RS_LXMF_VERSION = 053e357325f92e536f0ebbcaa43945a66c5ca3c0
 RS_LXMF_SITE = https://github.com/reticulum-spb/rsLXMF
 RS_LXMF_SITE_METHOD = git
 RS_LXMF_DEPENDENCIES = host-rustc rs-reticulum
--- Firmware/system/br2_external/package/rs-rrc-client/rs-rrc-client.mk
+++ Firmware/system/br2_external/package/rs-rrc-client/rs-rrc-client.mk
@@ -7,6 +7,7 @@
 RS_RRC_CLIENT_VERSION = c31425dd3a5fd5aba66528322cfaf84d4fa8665d
 RS_RRC_CLIENT_SITE = https://github.com/reticulum-spb/rsRRC-client
 RS_RRC_CLIENT_SITE_METHOD = git
+RS_RRC_CLIENT_DEPENDENCIES = rs-rrc
 
 define RS_RRC_CLIENT_CREATE_SYMLINK
     ln -sfn $(@D) $(BUILD_DIR)/rsRRC-client
